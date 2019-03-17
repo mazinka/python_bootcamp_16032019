99a5d5068989cae19edf37e085854f4f8dfe0d1a
mazinka <mazinka@poczta.onet.pl>
2019-03-17 09:48:49 +0100

    dodano zadanie drugie

---
+++ first.py
@@ -0,0 +1,32 @@
+import math
+10**10
+
+#działania na liczbach
+print("argumenty całkowite")
+print(10+10) #też można wpisać komentarz
+print(10-10)
+print(10*10)
+print(10/3)
+print(10//3)
+print(10%3)
+print(10**(0.5))
+print(math.sqrt(10))
+print()
+
+print("argumenty całkowite")
+print(10.0+10.0)
+print(10.0-10.0)
+print(10.0*10.0)
+print(10.0/3.0)
+print(10.0//3.0)
+print(10.0%3.0)
+print(10.0**(0.5))
+print(math.sqrt(10.0))
+print()
+
+
+print(((10+3)*17)/(20**3))
+print(10+3*17/20**3)
+
+
+print(1/2*)
---
+++ imie.py
@@ -0,0 +1,19 @@
+# ctr+alt+L formatuje mi kod
+
+# ex1
+imie = input("podaj imie: ")
+wzrost = input('podaj wzrost: ')
+
+print("imie" + str(imie))
+print("wzrost" + wzrost)
+
+# ex2
+imie = input("podaj imie: ")
+wzrost = input('podaj wzrost: ')
+waga = input('podaj wage')
+
+bmi = int(waga) / (float(wzrost) ** 2)
+
+print("imie" + str(imie))
+print("wzrost" + wzrost)
+print("bmi" + str(bmi))
